ab933bd3cbd7776d63061dc704cda41caa1221fb
Fariba DK <faribacodes@gmail.com>
2021-05-07 16:28:13 -0700

    Added new game route

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -4,20 +4,30 @@ const db = require('../models/index');
 const methodOverride = require('method-override');
 
 // Index
-router.get('/', async (req,res)=>{
-    const allGames= await db.Game.find();
-    res.render('games/game-index',{
-        games: allGames,
-    });
+router.get('/', async (req, res) => {
+  const allGames = await db.Game.find();
+  res.render('games/game-index', {
+    games: allGames,
+  });
 });
+
+//NEW
+router.get('/new', (req, res) => {
+  res.render('games/game-new');
+});
+
+
+
 // Show
-router.get('/:id', async (req,res)=>{
-    const selectedGame= await db.Game.findById({_id: req.params.id});
-    const averageRating= selectedGame.userRatings.reduce((acc,curr)=>acc+curr)/selectedGame.userRatings.length;
-    res.render('games/game-show',{
-        selected: selectedGame,
-        avgRating: averageRating.toFixed(2),
-    });
+router.get('/:id', async (req, res) => {
+  const selectedGame = await db.Game.findById({ _id: req.params.id });
+  const averageRating =
+    selectedGame.userRatings.reduce((acc, curr) => acc + curr) /
+    selectedGame.userRatings.length;
+  res.render('games/game-show', {
+    selected: selectedGame,
+    avgRating: averageRating.toFixed(2),
+  });
 });
 
 module.exports = router;
