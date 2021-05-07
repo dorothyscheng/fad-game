1e78bb8cdf4bf8d37038a38711742b3e3ba580ad
dcheng <cheng.dorothy.s@gmail.com>
2021-05-07 16:03:09 -0600

    Created game index view and route

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -3,4 +3,12 @@ const router = express.Router();
 const db = require('../models/index');
 const methodOverride = require('method-override');
 
+// Index
+router.get('/', async (req,res)=>{
+    const allGames= await db.Game.find();
+    res.render('games/game-index',{
+        games: allGames,
+    });
+});
+
 module.exports = router;
