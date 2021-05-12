67514cbad029ffa5d43ff367bef6feeb8e296915
Fariba DK <faribacodes@gmail.com>
2021-05-12 14:23:00 -0700

    footer and nav-bar user login/logout

--- server.js
+++ server.js
@@ -7,28 +7,29 @@ const usersController = require('./controllers/usersController');
 const reviewsController = require('./controllers/reviewsController');
 // const designerController = require('./controllers/designerController'); //STRETCH
 const db = require('./models');
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:false}));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 const secret = require('./secret');
-app.use(session({
+app.use(
+  session({
     secret: secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: (1000 * 3600 * 48) 
-    } 
-}));
-  
+      maxAge: 1000 * 3600 * 48,
+    },
+  })
+);
+
 /////////////////CSS
-const path=require('path');
-app.use(express.static(path.join(__dirname,'public')));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 ////////////////////////////////////////////
 
 app.use('/games', gamesController);
 app.use('/users', usersController);
-app.use('/reviews',reviewsController);
-
+app.use('/reviews', reviewsController);
 
 // ///////////Dummy Data
 // const tempGames = require('./models/tempGames');
@@ -46,34 +47,45 @@ app.use('/reviews',reviewsController);
 
 ////////////////////////////////////////////ROUTES
 // Home
-app.get('/', (req,res) => {
-    res.render('home.ejs');
+app.get('/', (req, res) => {
+  const accessUrl = '/users/logout';
+  const accessText = 'Logout';
+  if (!req.session.currentUser) {
+    accessUrl = '/users/login';
+    accessText = 'Login';
+    }
+    res.render('home.ejs', {
+      accessUrl: accessUrl,
+      accessText: accessText,
+    });
+//   console.log(accessUrl);
+//   console.log(accessText);
 });
 // Catch-all
-app.get('*',(req,res,next)=>{
-    const error= new Error;
-    error.statusCode=404;
-    next(error);
-})
+app.get('*', (req, res, next) => {
+  const error = new Error();
+  error.statusCode = 404;
+  next(error);
+});
 // Error handler
-app.use((err,req,res,next)=>{
-    let message="Something went wrong.";
-    if (!err.statusCode) err.statusCode=500;
-    if (err.statusCode===400) {
-        res.status(400);
-        message = "Looks like you missed a required field. Try again."
-    } else if (err.statusCode===404) {
-        res.status(404);
-        message = "That page doesn't exist."
-    } else if (err.statusCode===401) {
-        res.status(401);
-        message = "Not Authorized"
-    };
-    res.render('error',{
-        message: message,
-    });
-})
+app.use((err, req, res, next) => {
+  let message = 'Something went wrong.';
+  if (!err.statusCode) err.statusCode = 500;
+  if (err.statusCode === 400) {
+    res.status(400);
+    message = 'Looks like you missed a required field. Try again.';
+  } else if (err.statusCode === 404) {
+    res.status(404);
+    message = "That page doesn't exist.";
+  } else if (err.statusCode === 401) {
+    res.status(401);
+    message = 'Not Authorized';
+  }
+  res.render('error', {
+    message: message,
+  });
+});
 ////////////////////////////////////////////
 app.listen(PORT, () => {
-    console.log(`Server Active: http://localhost:${PORT}`);
+  console.log(`Server Active: http://localhost:${PORT}`);
 });
