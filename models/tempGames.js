a17cdf8345cdf57b3fa12aa22911911ad5043038
denise williams <denisewilliams@denises-MacBook-Pro.local>
2021-05-13 17:00:45 -0400

    css edits and text input changed to textarea

--- models/tempGames.js
+++ models/tempGames.js
@@ -1,7 +1,7 @@
 const tempGameArray = [
     {
         name: "Wingspan",
-        description: "Attract a beautiful and diverse collection of birds to your wildlife reserve.",
+        description: "Wingspan is a competitive, medium-weight, card-driven, engine-building board game from Stonemaier Games. It's designed by Elizabeth Hargrave and features over 170 birds illustrated by Natalia Rojas and Ana Maria Martinez.",
         numPlayers: {
             min: 1,
             max: 5
@@ -13,12 +13,11 @@ const tempGameArray = [
             },
         designer: ["Elizabeth Hargrave"],
         image: "https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__itemrep/img/DR7181wU4sHT6gn6Q1XccpPxNHg=/fit-in/246x300/filters:strip_icc()/pic4458123.jpg",
-        userRatings: [9,8,9,6,8,7,10,9,8],
         genre: ["Family","Strategy"]
     },
     {
         name: "Catan",
-        description: "Do you hate your friends? Time to prove it.",
+        description: "In CATAN (formerly The Settlers of Catan), players try to be the dominant force on the island of Catan by building settlements, cities, and roads. On each turn dice are rolled to determine what resources the island produces. Players build by spending resources (sheep, wheat, wood, brick and ore) that are depicted by these resource cards; each land type, with the exception of the unproductive desert, produces a specific resource: hills produce brick, forests produce wood, mountains produce ore, fields produce wheat, and pastures produce sheep.",
         numPlayers: {
             min: 3,
             max: 4
@@ -30,12 +29,11 @@ const tempGameArray = [
             },
         designer: ["Klaus Teuber"],
         image: "https://cf.geekdo-images.com/W3Bsga_uLP9kO91gZ7H8yw__itemrep/img/IzYEUm_gWFuRFOL8gQYqGm5gU6A=/fit-in/246x300/filters:strip_icc()/pic2419375.jpg",
-        userRatings: [7,10,9,8,2,5,1,3,7,5,3,7],
         genre: ["Strategy","Family"]
     },
     {
         name: "Voidfall",
-        description: "Fall through the void! How exciting.",
+        description: "Voidfall is a space 4X game that brings the genre to Euro enthusiasts' tables. It combines the tension, player interaction, and deep empire customization of the 4X genre with the resource management, tight decisions, and minimum-luck gameplay of an economic Euro. Win by pushing back the Voidborn in the solo/coop mode, or by overcoming your rivals' influence in restoring the Domineum in the competitive mode — both using the same rule set and game system. Variability is ensured not only by multiple playable houses with their own strengths and weaknesses, but also by many different map set-ups for all game modes.",
         numPlayers: {
             min: 1,
             max: 4
@@ -47,9 +45,126 @@ const tempGameArray = [
             },
         designer: ["Nigel Buckle","David Tuczi"],
         image: "https://cf.geekdo-images.com/hItZjdDTNuaCZ7fEztwcUQ__itemrep/img/Uzot_otbvYXpwkTpnc1uwiXUWqs=/fit-in/246x300/filters:strip_icc()/pic6153324.jpg",
-        userRatings: [7,5,3,7,10,1,3,4,8,2,5],
+        genre: ["Strategy, Sci-fi"]
+    },
+    {
+        name: "Beyond the Sun",
+        description: "Beyond the Sun is a space civilization game in which players collectively decide the technological progress of humankind at the dawn of the Spacefaring Era, while competing against each other to be the leading faction in economic development, science, and galactic influence.",
+        numPlayers: {
+            min: 2,
+            max: 4
+            },
+        ageRating: 14,
+        playTime: {
+            min: 60,
+            max: 120
+            },
+        designer: ["Dennis K. Chan"],
+        image: "https://cf.geekdo-images.com/BfEHqHQAvZLbRX7y7e9TWg__imagepage/img/ZykdvPrRZ-BIDA59HPviWMc9TcE=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5617866.jpg",
         genre: ["Strategy"]
-    }
+    },
+    {
+        name: "Sleeping Gods",
+        description: "In Sleeping Gods, you and up to 3 friends become Captain Sofi Odessa and her crew, lost in a strange world in 1929 on your steamship, the Manticore. You must work together to survive, exploring exotic islands, meeting new characters, and seeking out the totems of the gods so that you can return home.",
+        numPlayers: {
+            min: 1,
+            max: 4
+            },
+        ageRating: 13,
+        playTime: {
+            min: 60,
+            max: 1200
+            },
+        designer: ["Ryan Laukat"],
+        image: "https://cf.geekdo-images.com/Zdt8l4oTBpFICsMyNof7Jg__imagepage/img/ToI3nXdkLu4Dy3J8o-yAWrdN4i8=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5975244.png",
+        genre: ["Adventure, Exploration, Nautical"]
+    },
+    {
+        name: "Merchants Cove",
+        description: "Merchants Cove is a highly asymmetrical eurogame where each player assumes the role of a different fantasy merchant with a unique shop. The merchants contend to sell their goods to the arriving adventurers at the cove’s piers—the most famous markets in the Five Realms. Each player uses their own set of role-specific components and gameplay mechanisms to produce their goods, increase their shop’s efficiency, and—most importantly—get rich!",
+        numPlayers: {
+            min: 1,
+            max: 4
+            },
+        ageRating: 14,
+        playTime: {
+            min: 60,
+            max: 90
+            },
+        designer: ["Jonny Pac, Carl Van Ostrand, Drake Villareal"],
+        image: "https://cf.geekdo-images.com/-JD8JLncWYMes9Q6YIgorg__imagepage/img/mLS-oHXiPNRtqJSt-YQ36l0XXGM=/fit-in/900x600/filters:no_upscale():strip_icc()/pic6136538.jpg",
+        genre: ["Strategy, Fantasy, Nautical"]
+    },
+    {
+        name: "Destines",
+        description: `Destinies is a competitive, story-driven, game of adventure and exploration, mixing an app and a board game.
+
+        The first in a series of games using a brand-new system called Destinies. This game is set in a dark medieval-fantasy universe.
+        
+        The Destinies system offers a fully story-driven, app-supported, RPG-like board game experience, without the need for a game master. Each scenario depicts a part of a vivid world, full of dark stories, epic NPCs and mysteries to solve. Each player takes the role of a hero on a quest to fulfill their destiny. Each destiny is a final goal of the character and has at least two, completely different paths to victory, composed of branching series of quests. Players compete with each other to push the world towards their own destiny.`,
+        numPlayers: {
+            min: 1,
+            max: 3
+            },
+        ageRating: 14,
+        playTime: {
+            min: 90,
+            max: 150
+            },
+        designer: ["Michael G, Filip M"],
+        image: "https://cf.geekdo-images.com/oaD1ZQ3yGj6lacLdtqgdnQ__imagepage/img/fk6BConHqwDOSHeKbOavfrFs7Fo=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5558118.png",
+        genre: ["Adventure, Exploration, Fantasy"]
+    },
+    {
+        name: "My Father's Work",
+        description: "The walls were lined with iron shelves, each metal slat overfilled with glass jars containing formaldehyde and grotesque curiosities within. Pristine brass tools and refined metals of a quality I had never before laid eyes upon were strewn across sturdy slabs of rock and wood, their edges sharp with use. However, my eyes were soon drawn to a sturdy writing desk, its mahogany eaves inlaid with thin strips of copper, the center of which contained a well-worn leather-bound book. My father's journal — passed down to me and representing years of knowledge and countless experiments. And inside that weathered tome, atop the pearly parchment oxidized yellow at its frayed edges, were the deliberate quill marks of a crazed genius outlining the ambitious project he could never complete in one lifetime — his masterwork.",
+        numPlayers: {
+            min: 2,
+            max: 4
+            },
+        ageRating: 14,
+        playTime: {
+            min: 120,
+            max: 120
+            },
+        designer: ["T.C. Petty"],
+        image: "https://cf.geekdo-images.com/N0hCcxXw8d7vUjKfsYO6vg__imagepage/img/odLFLvl1daeCD7ox8sVuqQzDtxY=/fit-in/900x600/filters:no_upscale():strip_icc()/pic6121929.png",
+        genre: ["Horror"]
+    },
+    {
+        name: "Dune: Imperium",
+        description: `Dune: Imperium is a game that finds inspiration in elements and characters from the Dune legacy, both the new film from Legendary Pictures and the seminal literary series from Frank Herbert, Brian Herbert, and Kevin J. Anderson.
+
+        As a leader of one of the Great Houses of the Landsraad, raise your banner and marshal your forces and spies. War is coming, and at the center of the conflict is Arrakis – Dune, the desert planet.`,
+        numPlayers: {
+            min: 1,
+            max: 4
+            },
+        ageRating: 14,
+        playTime: {
+            min: 60,
+            max: 120
+            },
+        designer: ["Paul Dennen"],
+        image: "https://cf.geekdo-images.com/PhjygpWSo-0labGrPBMyyg__imagepage/img/BjM3LyahJ4IQ2ov5MkzkHatbmUc=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5666597.jpg",
+        genre: ["Strategy, Sci-fi"]
+    },
+    {
+        name: "Gloomhaven",
+        description: "Gloomhaven is a game of Euro-inspired tactical combat in a persistent world of shifting motives. Players will take on the role of a wandering adventurer with their own special set of skills and their own reasons for traveling to this dark corner of the world. Players must work together out of necessity to clear out menacing dungeons and forgotten ruins. In the process, they will enhance their abilities with experience and loot, discover new locations to explore and plunder, and expand an ever-branching story fueled by the decisions they make.",
+        numPlayers: {
+            min: 1,
+            max: 4
+            },
+        ageRating: 14,
+        playTime: {
+            min: 60,
+            max: 120
+            },
+        designer: ["Issac Childres"],
+        image: "https://cf.geekdo-images.com/sZYp_3BTDGjh2unaZfZmuA__imagepage/img/pBaOL7vV402nn1I5dHsdSKsFHqA=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2437871.jpg",
+        genre: ["Strategy, Adventure, Fantasy"]
+    },
 ]
 
 module.exports = tempGameArray;
