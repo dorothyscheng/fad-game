8db47b938566decba9b70a53152a441c43e42cde
dcheng <cheng.dorothy.s@gmail.com>
2021-05-14 16:02:10 -0600

    commented out home page

--- server.js
+++ server.js
@@ -53,106 +53,107 @@ app.use('/reviews', reviewsController);
 // );
 
 app.get('/', async (req, res) => {
-  const allGames= await db.Game.find().populate('reviews');
-  let allGameRatings=[];
-  let allGamesWithReviews=[];
+//   const allGames= await db.Game.find().populate('reviews');
+//   let allGameRatings=[];
+//   let allGamesWithReviews=[];
 
-  allGames.forEach(currentGame =>{
-    let ratingSum=0;
-    if (currentGame.reviews.length>0) {
-      allGamesWithReviews.push(currentGame);
-    };
-    currentGame.reviews.forEach(element=>{
-      ratingSum+=element.rating;
-    });
-    const averageRating=ratingSum/currentGame.reviews.length;
-    const currentGameObj={
-      name: currentGame.name,
-      id: currentGame._id,
-      image: currentGame.image,
-      avgRating: averageRating,
-    };
-    allGameRatings.push(currentGameObj);
-  });
+//   allGames.forEach(currentGame =>{
+//     let ratingSum=0;
+//     if (currentGame.reviews.length>0) {
+//       allGamesWithReviews.push(currentGame);
+//     };
+//     currentGame.reviews.forEach(element=>{
+//       ratingSum+=element.rating;
+//     });
+//     const averageRating=ratingSum/currentGame.reviews.length;
+//     const currentGameObj={
+//       name: currentGame.name,
+//       id: currentGame._id,
+//       image: currentGame.image,
+//       avgRating: averageRating,
+//     };
+//     allGameRatings.push(currentGameObj);
+//   });
   
-  // reference for sorting: https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
-  const sortedGames = allGameRatings.sort((a,b)=>{
-    return b.avgRating-a.avgRating;
-  });
-  let maxTopGames=9;
-  let topNineGames=[];
-  for (let i=0; i<maxTopGames; i++) {
-    topNineGames.push(sortedGames[i]);
-  };
+//   // reference for sorting: https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+//   const sortedGames = allGameRatings.sort((a,b)=>{
+//     return b.avgRating-a.avgRating;
+//   });
+//   let maxTopGames=9;
+//   let topNineGames=[];
+//   for (let i=0; i<maxTopGames; i++) {
+//     topNineGames.push(sortedGames[i]);
+//   };
 
-  const allUsers= await db.User.find();
-  let allUserReviewsCount=[];
+//   const allUsers= await db.User.find();
+//   let allUserReviewsCount=[];
 
-  allUsers.forEach(currentUser =>{
-    const currentUserObj={
-      username: currentUser.username,
-      id: currentUser._id,
-      profilePic: currentUser.profilePic,
-      numReviews: currentUser.reviews.length,
-    };
-    allUserReviewsCount.push(currentUserObj);
-  });
+//   allUsers.forEach(currentUser =>{
+//     const currentUserObj={
+//       username: currentUser.username,
+//       id: currentUser._id,
+//       profilePic: currentUser.profilePic,
+//       numReviews: currentUser.reviews.length,
+//     };
+//     allUserReviewsCount.push(currentUserObj);
+//   });
 
-  const sortedUsers = allUserReviewsCount.sort((a,b)=>{
-    return b.numReviews-a.numReviews;
-  });
-  let maxTopUsers=5;
-  if (sortedUsers.length<maxTopUsers) {
-    maxTopUsers=sortedUsers.length;
-  };
-  let topFiveUsers=[];
-  for (let i=0; i<maxTopUsers; i++) {
-    topFiveUsers.push(sortedUsers[i]);
-  };
+//   const sortedUsers = allUserReviewsCount.sort((a,b)=>{
+//     return b.numReviews-a.numReviews;
+//   });
+//   let maxTopUsers=5;
+//   if (sortedUsers.length<maxTopUsers) {
+//     maxTopUsers=sortedUsers.length;
+//   };
+//   let topFiveUsers=[];
+//   for (let i=0; i<maxTopUsers; i++) {
+//     topFiveUsers.push(sortedUsers[i]);
+//   };
 
-  const randomNumberGame= Math.floor(Math.random()*allGamesWithReviews.length);
-  const randomNumberReview=Math.floor(Math.random()*allGamesWithReviews[randomNumberGame].reviews.length);
-  const randomGameObj={
-    name: allGamesWithReviews[randomNumberGame].name,
-    review: allGamesWithReviews[randomNumberGame].reviews[randomNumberReview].review,
-    rating: allGamesWithReviews[randomNumberGame].reviews[randomNumberReview].rating,
-    id: allGamesWithReviews[randomNumberGame]._id,
-    image: allGamesWithReviews[randomNumberGame].image,
-  };
+//   const randomNumberGame= Math.floor(Math.random()*allGamesWithReviews.length);
+//   const randomNumberReview=Math.floor(Math.random()*allGamesWithReviews[randomNumberGame].reviews.length);
+//   const randomGameObj={
+//     name: allGamesWithReviews[randomNumberGame].name,
+//     review: allGamesWithReviews[randomNumberGame].reviews[randomNumberReview].review,
+//     rating: allGamesWithReviews[randomNumberGame].reviews[randomNumberReview].rating,
+//     id: allGamesWithReviews[randomNumberGame]._id,
+//     image: allGamesWithReviews[randomNumberGame].image,
+//   };
 
-  res.render('home.ejs', {
-    accessUrl: req.accessUrl,
-    accessText: req.accessText,
-    topNineGames: topNineGames,
-    topFiveUsers: topFiveUsers,
-    randomGame: randomGameObj,
-  });
-});
+//   res.render('home.ejs', {
+//     accessUrl: req.accessUrl,
+//     accessText: req.accessText,
+//     topNineGames: topNineGames,
+//     topFiveUsers: topFiveUsers,
+//     randomGame: randomGameObj,
+//   });
+// });
 
-app.get('*', (req, res, next) => {
-  const error = new Error();
-  error.statusCode = 404;
-  next(error);
-});
+// app.get('*', (req, res, next) => {
+//   const error = new Error();
+//   error.statusCode = 404;
+//   next(error);
+// });
 
-app.use((err, req, res, next) => {
-  let message = 'Something went wrong.';
-  if (!err.statusCode) err.statusCode = 500;
-  if (err.statusCode === 400) {
-    res.status(400);
-    message = 'Looks like you missed a required field. Try again.';
-  } else if (err.statusCode === 404) {
-    res.status(404);
-    message = "That page doesn't exist.";
-  } else if (err.statusCode === 401) {
-    res.status(401);
-    message = 'Not Authorized';
-  }
-  res.render('error', {
-    message: message,
-    accessUrl: req.accessUrl,
-    accessText: req.accessText,
-  });
+// app.use((err, req, res, next) => {
+//   let message = 'Something went wrong.';
+//   if (!err.statusCode) err.statusCode = 500;
+//   if (err.statusCode === 400) {
+//     res.status(400);
+//     message = 'Looks like you missed a required field. Try again.';
+//   } else if (err.statusCode === 404) {
+//     res.status(404);
+//     message = "That page doesn't exist.";
+//   } else if (err.statusCode === 401) {
+//     res.status(401);
+//     message = 'Not Authorized';
+//   }
+//   res.render('error', {
+//     message: message,
+//     accessUrl: req.accessUrl,
+//     accessText: req.accessText,
+//   });
+res.send('got it');
 });
 
 app.listen(PORT, () => {
